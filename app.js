f6fd5d09c020b40b431cb13972cf8bfa849d57d7
taiwang <cobra911@hotmail.com.tw>
2019-08-29 18:07:19 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Set constraints for the video stream
-var constraints = { video: { facingMode: { exact: "environment"}  }, audio: false };
+var constraints = { video: { facingMode: { exact: "user"}  }, audio: false };
 // Define constants
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
